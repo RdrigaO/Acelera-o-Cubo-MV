845b47b576eb7658f76fd51632537f3e5f14853c
rodrigo.jdasilva <rodrigo.jdasilva@mv.com.br>
2025-02-24 08:45:33 -0300

    Tratamento do http response e nomeclatura dos methods

--- neoGenesis/src/main/java/com/br/neoGenesis/api/controller/GameController.java
+++ neoGenesis/src/main/java/com/br/neoGenesis/api/controller/GameController.java
@@ -1,5 +1,8 @@
 package com.br.neoGenesis.api.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import com.br.neoGenesis.domain.model.Game;
 import com.br.neoGenesis.domain.service.GameService;
 import lombok.RequiredArgsConstructor;
@@ -9,29 +12,34 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1/games")
 @RequiredArgsConstructor
 public class GameController {
     private final GameService gameService;
 
-    @GetMapping("/listgames")
+    @Operation(summary = "List all games")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success to list all games"),
+            @ApiResponse(responseCode = "500", description = "Intern error in server")
+    })
+    @GetMapping
     public List<Game> listGames(){
-        return gameService.ListGames();
+        return gameService.listGames();
     }
     @GetMapping("/disponibles")
     public List<Game> listDisponibles(){
-        return gameService.ListDisponibleGames();
+        return gameService.listDisponibleGames();
     }
-    @PostMapping("/addgame")
+    @PostMapping
     public Game AddGame(@RequestBody Game game){
-        return gameService.SaveGame(game);
+        return gameService.saveGame(game);
     }
-    @DeleteMapping("/removegame/{id}")
+    @DeleteMapping("/{id}")
     public void DeleteGame(@PathVariable UUID id){
-        gameService.RemoveGame(id);
+        gameService.removeGame(id);
     }
-    @PutMapping("/updategame/{id}")
+    @PutMapping("/{id}")
     public Game UpdateGame(@PathVariable UUID id, @RequestBody Game updatedGame){
-        return gameService.UpdateGame(id, updatedGame);
+        return gameService.updateGame(id, updatedGame);
     }
 }
